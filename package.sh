0e4d02fe1116f118b7b5d24f70cd1a2884ffa17f
qzh731 <101468517+qzh731@users.noreply.github.com>
2026-01-17 15:55:50 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherap
 
 # Theme
 # luci-theme-neobird
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+# git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
 # Mentohust
 git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
